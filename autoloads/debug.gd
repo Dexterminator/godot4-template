96c47218922e63dab208f17d09cd5803e25fd16b
Dexter Gramfors <dexter.gramfors@gmail.com>
2024-06-26 22:20:17 -0400

    Add debug.gd

---
+++ autoloads/debug.gd
@@ -0,0 +1,22 @@
+extends Node
+var time_scale = 1
+
+
+func _process(_delta):
+	if OS.is_debug_build() and time_scale < 1:
+		Engine.time_scale = time_scale
+
+
+func _input(_delta):
+	if not OS.is_debug_build():
+		return
+
+	if not Input.is_key_pressed(KEY_ALT):
+		return
+
+	if Input.is_key_pressed(KEY_2):
+		if time_scale < 1:
+			time_scale = 1
+			Engine.time_scale = 1
+		else:
+			time_scale = 0.3
